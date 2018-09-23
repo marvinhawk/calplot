cf791b0ddd206d864270851f101658d658942bfb
marvinhawk <24456924+marvinhawk@users.noreply.github.com>
2018-09-23 18:46:27 +0200

    Plotting function split into individual functions for weeks and months for more control

--- calplot.py
+++ calplot.py
@@ -9,27 +9,40 @@ def month_table(ds):
 	rds = ds.reindex(full_range.index).fillna(value=0)
 	df = pd.DataFrame({"data": rds, "day": rds.index.day, "month": rds.index.month})
 	dfp = df.pivot(index="month", columns="day", values="data")
-	sns.set()
-	sns.set_style("whitegrid")
-	calplot(dfp)
+	months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+	month_plot(dfp, months)
 
 def week_table(ds):
 	full_range = range_generator(ds)
 	rds = ds.reindex(full_range.index)
 	df = pd.DataFrame({"data": rds, "day": rds.index.weekday, "week": rds.index.week})
 	dfp = df.pivot(index="week", columns="day", values="data")
-	sns.set()
-	calplot(dfp)
+	weekdays = ["M", "T", "W", "T", "F", "S", "S"]
+	week_plot(dfp, weekdays)
 
 def range_generator(ds):
 	full_range = pd.Series(index=pd.date_range(ds.index[0] - MonthBegin(), ds.index[-1] + MonthEnd()))
 	return full_range
 
-def calplot(df):
+def month_plot(df, ylabel="auto"):
+	ymax = len(df.index)
 	base_color = "#eaeaf2"
 	shading = int(df.max().max())
-	colors = sns.color_palette("OrRd", n_colors=shading)
+	colors = sns.color_palette("GnBu", n_colors=shading)
 	colors[0] = base_color
+	sns.set()
+	sns.set_style("whitegrid")
+	plt.figure(figsize=(6,2))
+	ax = sns.heatmap(df, cmap=colors, linewidth=.5, square=True, yticklabels=ylabel[:ymax])
+	plt.xlabel("Day")
+	plt.ylabel("Month")
+	plt.title("Month-indexed Heatmap")
+	plt.show()
 
-	ax = sns.heatmap(df, cmap=colors, linewidth=.5, square=True)
+def week_plot(df, xlabel="auto"):
+	sns.set()
+	ax = sns.heatmap(df, cmap="GnBu", linewidth=.5, square=True, xticklabels=xlabel)
+	plt.xlabel("Weekday")
+	plt.ylabel("Week")
+	plt.title("Week-indexed Heatmap")
 	plt.show()
